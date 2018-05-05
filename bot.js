319f00719cdb762ac2e1fd2d0eb0c5f06384339e
electricalice <39010957+electricalice@users.noreply.github.com>
2018-05-05 19:23:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,27 @@ client.on('message', message => {
   	}
 });
 
+
+client.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == 'Alberto') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'parlami':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Ciao! '.user
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
