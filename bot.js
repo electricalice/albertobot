f6aeffbe08b3ce7f09ee3ad1663517c7f0506c56
electricalice <39010957+electricalice@users.noreply.github.com>
2018-05-05 21:46:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,11 +50,6 @@ client.on('message', message => {
   	}
 });
 
-client.on('message', message => {
-    if (message.content.includes(nomebot)) {
-    	message.reply('State parlando di me?');
-  	}
-});
 
 client.on('message', message => {
     if (message.content.includes('GRAZIE ALBERTO')) {
