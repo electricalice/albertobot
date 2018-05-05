bca0b7fa11fa00ca4a53f1c446c12b000156b5c9
electricalice <39010957+electricalice@users.noreply.github.com>
2018-05-05 20:35:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,6 +32,12 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
+    if (message.content == nomebot + 'che ne pensi di Lavinia?') {
+    	message.reply('mmmmh.... non saprei. Non mi sembra molto intelligente.');
+  	}
+});
+
+client.on('message', message => {
     if (message.content == nomebot + 'dimmi qualcosa') {
         
         var risposte = [ 'Londra è nata dal nulla, non per motivi militari, ma essenzialmente per ragioni commerciali.', 'Dal momento che nessun sistema industriale può stare in piedi con un numero esiguo di consumatori, se non si crea un mercato attraverso l’aumento diffuso del reddito il sistema s’inceppa', 'Bere vino era equiparato a un adulterio. Il bacio insomma era… l’etilometro della fedeltà. L’equazione è semplice. Bere infatti era l’anticamera dell’adulterio. Perché una donna, bevendo, perdeva il controllo e poteva facilmente commettere un tradimento o, più in generale, comportarsi in modo disdicevole.', 'Kairós era la divinità… dell’attimo fuggente! Cioè dell’occasione da non perdere. ', 'Credo che la formica parassitaria sia l\'animale più longevo e insieme più debole che esista. La struttura esoscheletrica è del tutto simile a una zanzara in volo, e tuttavia con un bulbo parcellizzato più regolare.', 'Prima di Ciampi la patria era un valore di destra, guerresco. Lui ha convinto gli italiani a fare pace con la bandiera e con l\'inno.',  ];
